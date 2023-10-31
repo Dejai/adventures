b7dd66d6f50f31948f4158a6a8c271efadfb55fc
Derrick Fyfield <derrickfyfield@DerricksMacMini.local>
2023-10-31 18:20:18 -0400

    Adjust style

--- src/js/menu.js
+++ src/js/menu.js
@@ -7,43 +7,51 @@ const loadDropdownMenu = async (loginDetails) => {
 
 
 // Toggle the dropdown nav
-function toggleDropdownNav() {
+function toggleDropdownNav(state="") {
     var dropdownNav = document.querySelector("#dropdownNav");
     var isOpen = (dropdownNav.classList.contains("open"));
-    if(!isOpen)
-    {
-        dropdownNav.classList.add("open");
-        MyDom.hideContent("#hamburgerMenuIcon");
-        MyDom.showContent("#closeMenuIcon");
-        // MyDom.addClass(".toggleOnSideNavMobile", "navOpen");
-    } else {
-        dropdownNav.classList.remove("open");
-        MyDom.showContent("#hamburgerMenuIcon");
-        MyDom.hideContent("#closeMenuIcon");
-        // MyDom.removeClass(".toggleOnSideNavMobile", "navOpen");
+    state = (state != "") ? state : ( (!isOpen) ? "open" : "close" );
+    switch(state) {
+        case "open":
+            // Close the dropdown nave it happens to be open
+            toggleSearchBar("close");
+            // Show the section
+            dropdownNav.classList.add("open");
+            MyDom.hideContent("#hamburgerMenuIcon");
+            MyDom.showContent("#closeMenuIcon");
+            break;
+        default:
+            dropdownNav.classList.remove("open");
+            MyDom.showContent("#hamburgerMenuIcon");
+            MyDom.hideContent("#closeMenuIcon");    
+            break;
+
     }
 }
 
 // Toggle the search bar
-var toggleSearchBar = (icon) => {
-
+var toggleSearchBar = (state="") => {
     var isActive = document.querySelector("#searchBarSection")?.classList.contains("active");
-
-    //  If not active, open it (make it active);
-    if(!isActive){
-        // Focus on field if found
-         document.querySelector("#searchBarInput")?.focus();
-        // Adjust visibility of items with classes
-        MyDom.addClass("#searchBarSection", "active");
-        MyDom.addClass(".fadeOnSearch", "dtk-fade-out");
-        MyDom.hideContent(".hideOnSearch");
-    } else { 
-        // Find the button to clear search & click it
-        document.querySelector("#searchClearIcon")?.click();
-        // Control visibility with classes
-        MyDom.removeClass("#searchBarSection", "active");
-        MyDom.addClass(".fadeOnSearch", "dtk-fade-in");
-        MyDom.removeClass(".fadeOnSearch", "dtk-fade-out");
-        MyDom.showContent(".hideOnSearch");
+    state = (state != "") ? state : ( (!isActive) ? "open" : "close" );
+    switch(state) {
+        case "open":
+            // Close the dropdown nave it happens to be open
+            toggleDropdownNav("close");
+            // Focus on field if found
+            document.querySelector("#searchBarInput")?.focus();
+            // Adjust visibility of items with classes
+            MyDom.addClass("#searchBarSection", "active");
+            MyDom.addClass(".fadeOnSearch", "dtk-fade-out");
+            MyDom.hideContent(".hideOnSearch");
+            break;
+        default:
+            // Find the button to clear search & click it
+            document.querySelector("#searchClearIcon")?.click();
+            // Control visibility with classes
+            MyDom.removeClass("#searchBarSection", "active");
+            MyDom.addClass(".fadeOnSearch", "dtk-fade-in");
+            MyDom.removeClass(".fadeOnSearch", "dtk-fade-out");
+            MyDom.showContent(".hideOnSearch");
+            break;
     }
 }
